0a50f1b218ca22f495ea5b291ac2a12d721a1565
Julius Caezar <julius.caezar25@gmail.com>
2023-02-10 22:16:24 -0400

    creating component PaymentMethodOptionsContainer

--- src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
+++ src/pages/CompleteOrder/components/CompleteOrderForm/styles.ts
@@ -36,4 +36,8 @@ export const AddressFormContainer = styled.div`
   }
 `;
 
-//minuto 1:52:14
+export const PaymentMethodOptionsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 0.75rem;
+`;
