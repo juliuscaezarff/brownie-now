4ddadfd075d1ef004b47672deeb2efae05923147
Julius Caezar <julius.caezar25@gmail.com>
2023-02-13 19:59:12 -0400

    settings styles for BrownieCartCardContainer

--- src/pages/CompleteOrder/components/BrownieCartCard/styles.ts
+++ src/pages/CompleteOrder/components/BrownieCartCard/styles.ts
@@ -1,6 +1,32 @@
 import styled from "styled-components";
 
-export const BrownieCartCardContainer = styled.div``;
+export const BrownieCartCardContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-bottom: 1px solid ${({ theme }) => theme.colors["base-button"]};
+  padding-bottom: 1.5rem;
+  margin-bottom: 1.5rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 1.25rem;
+
+
+    img{
+      width: 4rem;
+      height: 4rem;
+    }
+  }
+
+  > p {
+    align-self: flex-start;
+    font-weight: 700;
+  }
+`;
 
 export const ActionsContainer = styled.div``;
 
