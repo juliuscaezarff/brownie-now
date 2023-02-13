fb05278ad76473d0baedc88e1f5a37e251bcb0b8
Julius Caezar <julius.caezar25@gmail.com>
2023-02-13 19:49:11 -0400

    creating const RemoveButton

--- src/pages/CompleteOrder/components/BrownieCartCard/styles.ts
+++ src/pages/CompleteOrder/components/BrownieCartCard/styles.ts
@@ -3,3 +3,5 @@ import styled from "styled-components";
 export const BrownieCartCardContainer = styled.div``;
 
 export const ActionsContainer = styled.div``;
+
+export const RemoveButton = styled.button``;
