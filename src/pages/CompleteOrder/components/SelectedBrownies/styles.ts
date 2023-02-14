ff34ead64929b80721bd7c7a288385b12b4c5a0f
Julius Caezar <julius.caezar25@gmail.com>
2023-02-13 20:42:22 -0400

    settings styles for DetailsContainer

--- src/pages/CompleteOrder/components/SelectedBrownies/styles.ts
+++ src/pages/CompleteOrder/components/SelectedBrownies/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { SectionBaseStyles } from "../../styles";
 
 export const SelectedBrowniesContainer = styled.div`
   display: flex;
@@ -6,3 +7,9 @@ export const SelectedBrowniesContainer = styled.div`
   gap: 0.75rem;
   width: 40rem;
 `;
+
+export const DetailsContainer = styled(SectionBaseStyles)`
+  border-radius: 6px 44px 6pxx 44px;
+  display: flex;
+  flex-direction: column;
+`
