1828bb45afb37594271a51b5d37f00559b811c3f
Julius Caezar <julius.caezar25@gmail.com>
2023-02-17 21:19:37 -0400

    creating CompleteOrderPage

--- src/pages/CompleteOrder/index.tsx
+++ src/pages/CompleteOrder/index.tsx
@@ -1,8 +1,10 @@
 import { CompleteOrderForm } from "./components/CompleteOrderForm"
+import { SelectedBrownies } from "./components/SelectedBrownies";
 import { CompleteOrderContainer } from "./styles";
 
 export function CompleteOrderPage() {
   return <CompleteOrderContainer className="container">
     <CompleteOrderForm />
+    <SelectedBrownies />
   </CompleteOrderContainer>
 }
