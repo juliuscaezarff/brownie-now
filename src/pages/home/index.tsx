0ec470e0a4c8647cfad52dcb62fc8877fad88956
Julius Caezar <julius.caezar25@gmail.com>
2023-04-07 19:02:35 -0400

    import OurBrownie component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,13 @@
+import { OurBrownies } from "../components/OurBrownies";
 import { Intro } from "../components/Intro";
 import { HomeContainer } from "./styles";
+import { Wave } from "../../components/Wave";
 
 export function HomePage() {
   return (
     <HomeContainer>
       <Intro />
+      <OurBrownies />
     </HomeContainer>
   )
 }
