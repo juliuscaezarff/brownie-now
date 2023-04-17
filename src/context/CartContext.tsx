e3e8bec4afc4f45991aa83e2c9570f9e81f3a01f
Julius Caezar <julius.caezar25@gmail.com>
2023-04-17 14:28:36 -0300

    create const brownieAlreadyExistsInCart

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,5 +1,6 @@
 import { Children, ReactNode, createContext, useState } from 'react'
 import { Brownie } from '../pages/components/BrownieCard'
+import { produce } from 'immer'
 
 export interface CartItem extends Brownie {
   quantity: number
@@ -7,6 +8,7 @@ export interface CartItem extends Brownie {
 
 interface CartContextType {
   cartItems: CartItem[]
+  addBrownietoCart: (brownie: CartItem) => void
 }
 
 interface CartContextProviderProps {
@@ -19,11 +21,25 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
 
   function addBrownietoCart(brownie: CartItem) {
-    
+    const brownieAlreadyExistsInCart = cartItems.findIndex(
+      cartItems => cartItems.id === brownie.id
+    )
+
+    const newCart = produce(cartItems, draft => {
+      if (brownieAlreadyExistsInCart < 0) {
+        draft.push(brownie)
+      } else {
+        draft[brownieAlreadyExistsInCart].quantity += brownie.quantity
+      }
+    })
+
+    setCartItems(newCart)
   }
 
+  console.log(cartItems)
+
   return (
-    <CartContext.Provider value={{ cartItems }}>
+    <CartContext.Provider value={{ cartItems, addBrownietoCart }}>
       {children}
     </CartContext.Provider>
   )
