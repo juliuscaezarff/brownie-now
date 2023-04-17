5717e2bc0df6a641f830fdc7427fbd79d73a21bd
Julius Caezar <julius.caezar25@gmail.com>
2023-04-17 20:44:33 -0300

    i creating function handleIncrease and handleDecrease

--- src/pages/CompleteOrder/components/BrownieCartCard/index.tsx
+++ src/pages/CompleteOrder/components/BrownieCartCard/index.tsx
@@ -7,16 +7,36 @@ import {
 } from './styles'
 import { Trash } from 'phosphor-react'
 import brownieImg from '../../../../assets/brownieImageCard.jpg'
+import { CartItem } from '../../../../context/CartContext'
+import { formatMoney } from '../../../../utils/formateMoney'
+import { useCart } from '../../../../hooks/useCart'
 
-export function BrownieCartCard() {
+interface BrownieCartCardProps {
+  brownie: CartItem
+}
+
+export function BrownieCartCard({ brownie }: BrownieCartCardProps) {
+  const { changeCartItemQuantity } = useCart()
+
+  function handleIncrease() {
+    changeCartItemQuantity(brownie.id, "increase")
+  }
+
+  function handleDecrease() {
+    changeCartItemQuantity(brownie.id, "decrease")
+  }
+
+  const brownieTotal = brownie.price * brownie.quantity
+
+  const formattedPrice = formatMoney(brownieTotal)
   return (
     <BrownieCartCardContainer>
       <div>
-        <img src={brownieImg} alt="" />
+        <img src={`/brownies/${brownie.photo}`} alt="" />
         <div>
-          <RegularText color="subtitle">Brownie Normal</RegularText>
+          <RegularText color="subtitle">{brownie.name}</RegularText>
           <ActionsContainer>
-            <QuantityInput size='small'/>
+            <QuantityInput size='small'  onIncrease={handleIncrease} onDecrease={handleDecrease} quantity={brownie.quantity}/>
             <RemoveButton>
               <Trash size={16} />
               Remover
@@ -25,7 +45,7 @@ export function BrownieCartCard() {
         </div>
       </div>
 
-      <p>R$ 9,90</p>
+      <p>R$ {formattedPrice}</p>
     </BrownieCartCardContainer>
   )
 }
