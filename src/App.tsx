b71f2998aecf337f12a5d3b3b3a6f0866dd1b37b
Julius Caezar <julius.caezar25@gmail.com>
2023-04-17 21:00:28 -0300

    import CartContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { Router } from './Routes'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme/default'
+import { CartContextProvider } from './context/CartContext'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-
-      <Router />
+        <CartContextProvider>
+          <Router />
+        </CartContextProvider>
     </ThemeProvider>
   )
 }
