64a5391f6b11bd99c6effd0fbf12d0f153777fd9
Julius Caezar <julius.caezar25@gmail.com>
2023-05-09 17:31:13 -0300

    creating the order context

--- src/pages/CompleteOrder/components/SelectedBrownies/ConfirmationSection.tsx
+++ src/pages/CompleteOrder/components/SelectedBrownies/ConfirmationSection.tsx
@@ -1,30 +1,42 @@
 import { RegularText } from '../../../../components/Typegraphy'
 import { Button } from "../../../../components/Button";
 import { ConfirmationSectionContainer } from './styles'
+import { useCart } from '../../../../hooks/useCart';
+import { formatMoney } from '../../../../utils/formateMoney';
+
+const DELIVERY_PRICE = 3.9
 
 export function ConfirmantionSection() {
+  const { cartItemsTotal, cartQuantity } = useCart();
+  const cartTotal = DELIVERY_PRICE + cartItemsTotal;
+
+  const formattedItemsTotal = formatMoney(cartItemsTotal);
+  const formattedCartTotal = formatMoney(cartTotal);
+  const formattedDeliveryPrice = formatMoney(DELIVERY_PRICE)
+
   return (
     <ConfirmationSectionContainer>
       <div>
         <RegularText>Total de itens</RegularText>
-        <RegularText>R$</RegularText>
+        <RegularText>R$ {formattedItemsTotal}</RegularText>
       </div>
       <div>
         <RegularText size="s">Entrega</RegularText>
-        <RegularText>R$</RegularText>
+        <RegularText>R$ {formattedDeliveryPrice}</RegularText>
       </div>
       <div>
         <RegularText weight="700" color="subtitle" size="l">
           Total
         </RegularText>
         <RegularText weight="700" color="subtitle" size="l">
-          R$
+          R$ {formattedCartTotal}
         </RegularText>
       </div>
 
       <Button 
         text="Confirmar Pedido"
         type="submit"
+        disabled={cartQuantity <=0}
       />
     </ConfirmationSectionContainer>
   )
