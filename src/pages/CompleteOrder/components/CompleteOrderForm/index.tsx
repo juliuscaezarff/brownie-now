87b810260bc17ac61987e6bbbc3b9edd9f51d164
julius caezar <julius.caezar25@gmail.com>
2023-08-03 15:43:32 -0400

    feat: criando os inputs de cadastro de informações

--- src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,39 +1,39 @@
-import { TitleText } from "../../../../components/Typegraphy"
-import { SectionTitle } from "../SectionTitle"
-import { CompleteOrderFormContainer, FormSectionContainer } from "./styles";
-import { MapPinLine, CurrencyDollar } from 'phosphor-react';
-import { useTheme } from "styled-components";
-import { AddressForm } from "./AddressForm";
-import { PaymentMethodOptions } from "./PaymentMethodOptions";
+import { TitleText } from '../../../../components/Typegraphy'
+import { SectionTitle } from '../SectionTitle'
+import { CompleteOrderFormContainer, FormSectionContainer } from './styles'
+import { MapPinLine, CurrencyDollar } from 'phosphor-react'
+import { useTheme } from 'styled-components'
+import { AddressForm } from './AddressForm'
+import { PaymentMethodOptions } from './PaymentMethodOptions'
 
 export function CompleteOrderForm() {
-  const { colors } = useTheme(); 
+  const { colors } = useTheme()
 
   return (
-     <CompleteOrderFormContainer>
-      <TitleText>
-        Complete seu pedido
-      </TitleText>
+    <CompleteOrderFormContainer>
+      <TitleText>Complete seu pedido</TitleText>
 
       <FormSectionContainer>
         <SectionTitle
           title="Endereço de Entrega"
           subtitle="Informe o endereço onde deseja receber seu pedido"
-          icon={<MapPinLine color={colors["brand-yellow-dark"]} size={22}/>}
+          icon={<MapPinLine color={colors['brand-yellow-dark']} size={22} />}
         />
 
         <AddressForm />
       </FormSectionContainer>
-      
+
       <FormSectionContainer>
         <SectionTitle
           title="Pagamento"
           subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar."
-          icon={<CurrencyDollar color={colors["brand-purple-dark"]} size={22}/>}
+          icon={
+            <CurrencyDollar color={colors['brand-purple-dark']} size={22} />
+          }
         />
 
         <PaymentMethodOptions />
       </FormSectionContainer>
-     </CompleteOrderFormContainer>
+    </CompleteOrderFormContainer>
   )
 }
