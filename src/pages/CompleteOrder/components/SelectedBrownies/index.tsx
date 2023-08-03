d9f01f2121e13e60c041ccab2001909305e91e5a
julius caezar <julius.caezar25@gmail.com>
2023-08-03 15:47:12 -0400

    feat: usando o hook useCart e criando um map no component BrownieCartCard

--- src/pages/CompleteOrder/components/SelectedBrownies/index.tsx
+++ src/pages/CompleteOrder/components/SelectedBrownies/index.tsx
@@ -1,9 +1,11 @@
 import { TitleText } from '../../../../components/Typegraphy'
+import { useCart } from '../../../../hooks/useCart'
 import { BrownieCartCard } from '../BrownieCartCard'
 import { ConfirmantionSection } from './ConfirmationSection'
 import { DetailsContainer, SelectedBrowniesContainer } from './styles'
 
 export function SelectedBrownies() {
+  const { cartItems } = useCart()
   return (
     <SelectedBrowniesContainer>
       <TitleText size="xs" color="subtitle">
@@ -11,11 +13,11 @@ export function SelectedBrownies() {
       </TitleText>
 
       <DetailsContainer>
-        <BrownieCartCard />
-        <BrownieCartCard />
-        <BrownieCartCard />
+        {cartItems.map((item) => (
+           <BrownieCartCard key={item.id} brownie={item}/>
+        ))}
 
-        <ConfirmantionSection  />
+        <ConfirmantionSection />
       </DetailsContainer>
     </SelectedBrowniesContainer>
   )
