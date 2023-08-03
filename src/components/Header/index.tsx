ca9715225f63da15e907c4af0f01412466f4f250
julius caezar <julius.caezar25@gmail.com>
2023-08-03 14:53:47 -0400

    feat: usando o hook do cart e criando uma nova logo para o site

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,31 @@
-import { HeaderButton, HeaderButtonContainer, HeaderContainer } from "./styles";
-import { MapPin, ShoppingCart } from "phosphor-react"
-import coffeeLogoImg from "../../assets/coffee-delivery-logo.svg";
-import { NavLink } from "react-router-dom"
+import { HeaderButton, HeaderButtonContainer, HeaderContainer } from './styles'
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import coffeeLogoImg from '../../assets/coffee-delivery-logo.svg'
+import { NavLink } from 'react-router-dom'
+import { useCart } from '../../hooks/useCart';
 
 export function Header() {
+  const { cartQuantity } = useCart();
   return (
     <HeaderContainer>
       <div className="container">
         <NavLink to="/">
-        {/* <img src={coffeeLogoImg} alt="" /> */}
-        <h1>brownie<span>NOW</span></h1>
+          {/* <img src={coffeeLogoImg} alt="" /> */}
+          <h1>
+            brownie<span>NOW</span>
+          </h1>
         </NavLink>
         <HeaderButtonContainer>
           <HeaderButton variant="white">
-            <MapPin size={20} weight="fill"/>
-            Humaitá, AM 
+            <MapPin size={20} weight="fill" />
+            Humaitá, AM
           </HeaderButton>
           <NavLink to="/completeOrder">
-          <HeaderButton variant="brown">
-            <ShoppingCart size={20} weight="fill"/>
+            <HeaderButton variant="brown">
+              {cartQuantity >= 1 && <span>{cartQuantity}</span>}
+              <ShoppingCart size={20} weight="fill" />
             </HeaderButton>
-            </NavLink>
+          </NavLink>
         </HeaderButtonContainer>
       </div>
     </HeaderContainer>
