acaca4e0a10bfd761a1d87be8ca3c937c5339f47
julius caezar <julius.caezar25@gmail.com>
2023-08-07 16:18:00 -0400

    feat: criando o input do tipo ratio da seção de pagamento

--- src/pages/CompleteOrder/components/PaymentMethodInput/index.tsx
+++ src/pages/CompleteOrder/components/PaymentMethodInput/index.tsx
@@ -1,9 +1,24 @@
-import { PaymentMethodContainer } from './styles'
-import { CreditCard } from 'phosphor-react'
+import { forwardRef, InputHTMLAttributes, ReactNode } from "react";
+import { ContentContainer, PaymentMethodContainer } from "./styles";
 
-export function PaymentMethodInput() {
-  return <PaymentMethodContainer>
-    <CreditCard size={16}/>
-    Cartão de crédito
-  </PaymentMethodContainer>
-}
+type PaymentMethodInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  icon: ReactNode;
+  label: string;
+};
+
+export const PaymentMethodInput = forwardRef<
+  HTMLInputElement,
+  PaymentMethodInputProps
+>(({ id, icon, label, ...props }, ref) => {
+  return (
+    <PaymentMethodContainer>
+      <input id={id} type="radio" {...props} name="paymentMethod" ref={ref} />
+      <label htmlFor={id}>
+        <ContentContainer>
+          {icon}
+          {label}
+        </ContentContainer>
+      </label>
+    </PaymentMethodContainer>
+  );
+});
