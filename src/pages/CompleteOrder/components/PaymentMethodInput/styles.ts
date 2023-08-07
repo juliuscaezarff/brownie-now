872d248891cd87c43b3fa1784407e30c92eac81d
julius caezar <julius.caezar25@gmail.com>
2023-08-07 16:19:00 -0400

    feat: criando a estilização para o novo input de pagamento

--- src/pages/CompleteOrder/components/PaymentMethodInput/styles.ts
+++ src/pages/CompleteOrder/components/PaymentMethodInput/styles.ts
@@ -1,9 +1,29 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components'
 
 export const PaymentMethodContainer = styled.div`
+  input {
+    visibility: hidden;
+    appearance: none;
+  }
+
+  input:checked + label div {
+    ${({ theme }) => css`
+      background: ${theme.colors["brand-brown"]};
+      border-color: ${theme.colors["base-white"]};
+      color: ${theme.colors["base-white"]};
+
+      &:hover{
+        background: ${theme.colors["brand-brown"]};
+        
+      }
+    `}
+  }
+`
+
+export const ContentContainer = styled.div`
   padding: 0 1rem;
-  background: ${({ theme }) => theme.colors["base-button"]};
-  color: ${({ theme }) => theme.colors["base-text"]};
+  background: ${({ theme }) => theme.colors['base-button']};
+  color: ${({ theme }) => theme.colors['base-text']};
   display: flex;
   align-items: center;
   justify-content: flex-start;
@@ -12,16 +32,16 @@ export const PaymentMethodContainer = styled.div`
   text-transform: uppercase;
   border-radius: 6px;
   height: 3rem;
-  border: 1px solid ${({ theme }) => theme.colors["base-button"]};
+  border: 1px solid ${({ theme }) => theme.colors['base-button']};
   transition: 0.4s;
 
   svg {
-    color: ${({ theme }) => theme.colors["brand-purple"]};
+    color: ${({ theme }) => theme.colors['brand-purple']};
   }
 
   &:hover {
-    background: ${({ theme }) => theme.colors["base-hover"]};
+    background: ${({ theme }) => theme.colors['base-hover']};
   }
 
   user-select: none;
-`;
+`
