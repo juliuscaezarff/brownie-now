f707c82adf4c7cc121fb58df175d687b14bcfa62
julius caezar <julius.caezar25@gmail.com>
2023-08-07 18:26:48 -0400

    feat: criando a mobile version

--- src/pages/CompleteOrder/styles.ts
+++ src/pages/CompleteOrder/styles.ts
@@ -1,11 +1,19 @@
 import styled from "styled-components";
 
 export const CompleteOrderContainer = styled.form `
-
   margin-top: 2.5rem;
   display: flex;
   justify-content: space-between;
   gap: 2rem;
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    align-items: center; 
+    
+    > h1 {
+      text-align: center;
+    }
+  }
 `;
 
 export const SectionBaseStyles = styled.div`
