a2f77187841032552c4b6f39d63ccd4e76b596a4
julius caezar <julius.caezar25@gmail.com>
2023-08-08 09:43:36 -0400

    refactor: corrigindo a description

--- src/data/index.ts
+++ src/data/index.ts
@@ -9,7 +9,7 @@ export const brownies = [
   },
   {
     id: 2,
-    tags: ["tradicional"],
+    tags: ["recheado"],
     name: "Brownie Recheado",
     description: "Brownie com recheio de chocolate, mais chocolate",
     photo: "recheado.jpg",
@@ -18,7 +18,7 @@ export const brownies = [
   {
     id: 3,
     tags: ["tradicional"],
-    name: "Browne com Cobertura",
+    name: "Brownie com Cobertura",
     description: "Brownie com cobertura de chocolate amargo",
     photo: "cobertura.jpg",
     price: 9.9,
